4b7fb8c74b3b625fde679d37970e1a77fad5efcd
Brian Ngugi <brianngugi.sw@gmail.com>
2023-09-19 10:47:42 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+      let result
+  if (feet <= 400){
+      result  = 'This one is on me!'
+      return result
+  } 
+  else if (feet>2500){
+    return 'No can do.'
+  }
+  else if(feet > 2000){
+    result='I will gladly take your thirty bucks.'
+    return result
+  }
 }
 
-function ternaryCheckCity(){
+// input as a parameter
+// check if input is gt 400
+// return result
+console.log(scuberGreetingForFeet(2501));
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+      let result
+     return city === 'NYC'?'Ok, sounds good.' :'No go.'
+
+
+// input as parameter
+// check if input is NYC
+// return result 
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch(tip){
+      case 'generous':
+        return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'      
+      default :
+        return 'Bye.'
+}
+
+
+
+
+  //input as parameter
+  //check if tip is generous or not
+  //return appropriate result
 }
